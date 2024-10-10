cf2a80866508f3249ec40fcef68116ceaffdafcb
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-10-10 12:49:30 -0500

    first commit

---
+++ src/main/java/api/azure/msv_wikimedia_consumer/MsvWikimediaConsumerApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_wikimedia_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvWikimediaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvWikimediaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/azure/msv_wikimedia_consumer/MsvWikimediaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.msv_wikimedia_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvWikimediaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
